891f24fb9b8c8618c5d8027b56718ede77d0bf7d
vargoalfonso515 <vargoalfonso10@gmail.com>
2022-06-30 01:42:22 +0700

    Initial Commit

---
+++ mapsLoc/mapsLocApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapsLocApp.swift
+//  mapsLoc
+//
+//  Created by Vargo Alfonso on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct mapsLocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
